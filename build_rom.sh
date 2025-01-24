d1bd993ea673b3d7aec8a3e5007810acf9102ad2
maximka300 <klimashevskiymaxim2003@gmail.com>
2025-01-24 22:59:00 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b udc -g default,-mips,-darwin,-notdefault
 git clone https://github.com/maximka300/LocalManifest.git --depth 1 -b EvolutionX-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
