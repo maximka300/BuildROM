6e34fe74a8a796fd2e4ada74a3e9227e02dc7e7f
maximka300 <klimashevskiymaxim2003@gmail.com>
2025-01-24 22:58:30 +0300

    Create build_rom.sh

---
+++ build_rom.sh
@@ -0,0 +1,9 @@
+# sync rom
+repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/maximka300/LocalManifest.git --depth 1 -b EvolutionX-14 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+. build/envsetup.sh
+lunch lineage_lavender-userdebug
+m evolution
