83da6fb0d6641a057754fb5411d11dc44d70d963
SenseiDEVLinux <klimashevskiymaxim2003@gmail..com>
2025-02-07 22:20:16 +0300

    lavender: overlay: Convert overlays to RRO

--- build_rom.sh
+++
@@ -1,9 +0,0 @@
-# sync rom
-repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/maximka300/LocalManifest.git --depth 1 -b Evo14 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom
-. build/envsetup.sh
-lunch lineage_lavender-userdebug
-m evolution
